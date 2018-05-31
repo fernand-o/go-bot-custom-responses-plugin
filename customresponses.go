925cbf602ed8bdc4ba40b7b58eb55555c65d8072
Fernando Almeida <fernandoprsbr@gmail.com>
2018-05-30 22:02:46 -0300

    Extract messages to another file

--- customresponses.go
+++ customresponses.go
@@ -12,50 +12,6 @@ import (
 	"github.com/go-redis/redis"
 )
 
-const (
-	invalidArguments = "Please inform the params, ex:"
-)
-
-var matchExample = strings.Join([]string{
-	"!responses match set \"Is someone there?\" \"Hello\"",
-	"!responses match set \"Rick Sanchez\" \"@rick\" #fun",
-	"!responses match unset 1",
-	"!responses match showall",
-}, "\n")
-
-var argumentsMatchExample = strings.Join([]string{
-	"Usage:",
-	"```",
-	matchExample,
-	"```",
-}, "\n")
-
-var listExample = strings.Join([]string{
-	"!responses list add #fun \"Hey %s, looks like you're guilty\"",
-	"!responses list add #fun \"Is that you %s?\"",
-	"!responses list show #fun",
-	"!responses list showall",
-	"!responses list remove #fun \"Is that you %s?\"",
-	"!responses list delete #fun",
-}, "\n")
-
-var argumentsListExample = strings.Join([]string{
-	"Usage:",
-	"```",
-	listExample,
-	"```",
-}, "\n")
-
-var argumentsGeneralExample = strings.Join([]string{
-	"```",
-	"Defining matches and responses:",
-	matchExample,
-	"",
-	"Defining lists to be used within the responses:",
-	listExample,
-	"```",
-}, "\n")
-
 type Match struct {
 	key      string
 	match    string
@@ -98,50 +54,6 @@ func loadMatches() {
 		match.key = key
 		Matches = append(Matches, match)
 	}
-}
-
-func matchesKeyFmt(sufix string) string {
-	return "matches:" + sufix
-}
-
-func userMessageSetResponse(match, response string) string {
-	return fmt.Sprintf("Ok! I will send a message with %s when i found any occurences of %s", response, match)
-}
-
-func userMessageUnsetResponse(match string) string {
-	return fmt.Sprintf("Done, i'll not say anything more related to %s", match)
-}
-
-func userMessageNoResposesDefined() string {
-	return fmt.Sprintf("There are no responses defined yet. \n %s", argumentsMatchExample)
-}
-
-func userMessageResponsesDeleted() string {
-	return "All responses were deleted."
-}
-
-func userMessageListMessageAdded(list, message string) string {
-	return fmt.Sprintf("The message `%s` was added to the list `%s`.", message, list)
-}
-
-func userMessageListMessageRemoved(list, message string) string {
-	return fmt.Sprintf("The message `%s` was removed of the list `%s`.", message, list)
-}
-
-func userMessageListDeleted(list string) string {
-	return fmt.Sprintf("The list %s was deleted.", list)
-}
-
-func userMessageNoListsDefined() string {
-	return fmt.Sprintf("There are no lists defined yet. \n %s", argumentsListExample)
-}
-
-func userMessageListInvalidName() string {
-	return "The list name must starts with #"
-}
-
-func userMessageDBErased() string {
-	return "Database erased."
 }
 
 func showOrClearResponses(param string) (msg string) {
